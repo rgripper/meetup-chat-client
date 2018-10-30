d2ccb1217f65a31d9039b0c920252a705731ae70
Vladimir Zotov <azuresplash@gmail.com>
2018-10-30 22:41:25 +1100

    ChatClient public instance members are now bound

--- dist/ChatClient.d.ts
+++ dist/ChatClient.d.ts
@@ -1,17 +1,17 @@
-import { BehaviorSubject } from 'rxjs';
-import { ChatStateReducer } from './chatStateReducer';
-import { ClientState } from './ClientState';
+import { BehaviorSubject } from "rxjs";
+import { ChatStateReducer } from "./chatStateReducer";
+import { ClientState } from "./ClientState";
 export declare class ChatClient {
     private socket;
     stateChanges: BehaviorSubject<ClientState>;
     private emitCommand;
     static connect(url: string): ChatClient;
     constructor(url: string, chatStateReducer: ChatStateReducer);
-    tryLogin(userName: string): void;
-    logout(): void;
-    sendText(text: string): void;
-    resetState(): void;
-    connect(): void;
-    disconnect(): void;
+    tryLogin: (userName: string) => void;
+    logout: () => void;
+    sendText: (text: string) => void;
+    resetState: () => void;
+    connect: () => void;
+    disconnect: () => void;
     private wireEvents;
 }
--- src/ChatClient.ts
+++ src/ChatClient.ts
@@ -1,96 +1,120 @@
-import * as io from 'socket.io-client';
-import { SocketState } from './SocketState';
-import { BehaviorSubject } from 'rxjs';
-import { WebSocketEventName } from './shared/transport/WebSocketEventName';
-import { ClientCommand, ClientCommandType } from './shared/ClientCommand';
-import { ServerEvent } from './shared/ServerEvent';
-import { ChatStateReducer, chatStateReducer } from './chatStateReducer';
-import { ClientState, Authenticatable, AuthenticatableState } from './ClientState';
-import { UserChatState } from './shared/model/UserChatState';
+import * as io from "socket.io-client";
+import { SocketState } from "./SocketState";
+import { BehaviorSubject } from "rxjs";
+import { WebSocketEventName } from "./shared/transport/WebSocketEventName";
+import { ClientCommand, ClientCommandType } from "./shared/ClientCommand";
+import { ServerEvent } from "./shared/ServerEvent";
+import { ChatStateReducer, chatStateReducer } from "./chatStateReducer";
+import {
+  ClientState,
+  Authenticatable,
+  AuthenticatableState
+} from "./ClientState";
+import { UserChatState } from "./shared/model/UserChatState";
 
 export class ChatClient {
-    private socket: SocketIOClient.Socket;
+  private socket: SocketIOClient.Socket;
 
-    public stateChanges = new BehaviorSubject<ClientState>(ClientState.Initial);
+  public stateChanges = new BehaviorSubject<ClientState>(ClientState.Initial);
 
-    private emitCommand(command: ClientCommand) {
-        this.socket.emit(WebSocketEventName.ClientCommand, command);
-    }
+  private emitCommand(command: ClientCommand): void {
+    this.socket.emit(WebSocketEventName.ClientCommand, command);
+  }
 
-    static connect(url: string): ChatClient {
-        return new ChatClient(url, chatStateReducer);
-    }
+  static connect(url: string): ChatClient {
+    return new ChatClient(url, chatStateReducer);
+  }
 
-    constructor(url: string, chatStateReducer: ChatStateReducer) {
-        const socket = io(url, { transports: ['websocket'], autoConnect: false });
-        socket.on('connect', () => console.log('connected'));
-        socket.on('disconnect', () => console.log('disconnected'));
+  constructor(url: string, chatStateReducer: ChatStateReducer) {
+    const socket = io(url, { transports: ["websocket"], autoConnect: false });
+    socket.on("connect", () => console.log("connected"));
+    socket.on("disconnect", () => console.log("disconnected"));
 
-        this.socket = socket;
-        this.wireEvents(socket, state => this.stateChanges.next(state), () => this.stateChanges.getValue(), chatStateReducer);
-        this.socket.open();
-    }
+    this.socket = socket;
+    this.wireEvents(
+      socket,
+      state => this.stateChanges.next(state),
+      () => this.stateChanges.getValue(),
+      chatStateReducer
+    );
+    this.socket.open();
+  }
 
-    tryLogin(userName: string): void {
-        this.emitCommand({ type: ClientCommandType.TryLogin, userName });
-    }
+  tryLogin = (userName: string): void => {
+    this.emitCommand({ type: ClientCommandType.TryLogin, userName });
+  };
 
-    logout(): void {
-        this.emitCommand({ type: ClientCommandType.Logout });
-    }
+  logout = (): void => {
+    this.emitCommand({ type: ClientCommandType.Logout });
+  };
 
-    sendText(text: string): void {
-        this.emitCommand({ type: ClientCommandType.AddMessage, message: { text } });
-    }
+  sendText = (text: string): void => {
+    this.emitCommand({ type: ClientCommandType.AddMessage, message: { text } });
+  };
 
-    resetState(): void {
-        this.emitCommand({ type: ClientCommandType.ResetState });
-    }
+  resetState = (): void => {
+    this.emitCommand({ type: ClientCommandType.ResetState });
+  };
 
-    connect() {
-        this.stateChanges.next({ socket: SocketState.Connecting, chat: AuthenticatableState.NotAuthenticated });
-        this.socket.open();
-    }
+  connect = (): void => {
+    this.stateChanges.next({
+      socket: SocketState.Connecting,
+      chat: AuthenticatableState.NotAuthenticated
+    });
+    this.socket.open();
+  };
 
-    disconnect(): void {
-        this.socket.disconnect();
-    }
+  disconnect = (): void => {
+    this.socket.disconnect();
+  };
 
-    private wireEvents(socket: SocketIOClient.Socket, emitState: (state: ClientState) => void, getState: () => ClientState, chatStateReducer: ChatStateReducer): void {
-        const emitErrorState = (error: any) => emitState({
-            chat: AuthenticatableState.NotAuthenticated,
-            socket: { ...SocketState.Disconnected, error: error.toString() }
-        });
-        const emitLoggedOutState = () => emitState({
-            socket: SocketState.Connected,
-            chat: AuthenticatableState.NotAuthenticated
-        });
-        const emitPendingState = () => emitState({
-            socket: SocketState.Connecting,
-            chat: AuthenticatableState.NotAuthenticated
-        });
-        const emitDisconnectedState = () => emitState({
-            socket: SocketState.Disconnected,
-            chat: AuthenticatableState.NotAuthenticated
-        });
+  private wireEvents(
+    socket: SocketIOClient.Socket,
+    emitState: (state: ClientState) => void,
+    getState: () => ClientState,
+    chatStateReducer: ChatStateReducer
+  ): void {
+    const emitErrorState = (error: any) =>
+      emitState({
+        chat: AuthenticatableState.NotAuthenticated,
+        socket: { ...SocketState.Disconnected, error: error.toString() }
+      });
+    const emitLoggedOutState = () =>
+      emitState({
+        socket: SocketState.Connected,
+        chat: AuthenticatableState.NotAuthenticated
+      });
+    const emitPendingState = () =>
+      emitState({
+        socket: SocketState.Connecting,
+        chat: AuthenticatableState.NotAuthenticated
+      });
+    const emitDisconnectedState = () =>
+      emitState({
+        socket: SocketState.Disconnected,
+        chat: AuthenticatableState.NotAuthenticated
+      });
 
-        socket.on('error', emitErrorState);
-        socket.on('connect_error', emitErrorState);
-        socket.on('reconnect_error', emitErrorState);
-        socket.on('connect', emitLoggedOutState);
-        socket.on('reconnecting', emitPendingState);
-        socket.on('disconnect', emitDisconnectedState);
+    socket.on("error", emitErrorState);
+    socket.on("connect_error", emitErrorState);
+    socket.on("reconnect_error", emitErrorState);
+    socket.on("connect", emitLoggedOutState);
+    socket.on("reconnecting", emitPendingState);
+    socket.on("disconnect", emitDisconnectedState);
 
-        socket.on(WebSocketEventName.ServerEvent, (event: ServerEvent) => {
-            const state = getState();
-            console.log(event);
+    socket.on(WebSocketEventName.ServerEvent, (event: ServerEvent) => {
+      const state = getState();
+      console.log(event);
 
-            if (!state.socket.isConnected) return;
+      if (!state.socket.isConnected) return;
 
-            emitState({
-                socket: SocketState.Connected,
-                chat: chatStateReducer(state.chat as Authenticatable<UserChatState>, event)
-            });
-        });
-    }
+      emitState({
+        socket: SocketState.Connected,
+        chat: chatStateReducer(
+          state.chat as Authenticatable<UserChatState>,
+          event
+        )
+      });
+    });
+  }
 }
